c54c2c7540d6f9fb76ed358aa8d9d36782c60c6d
kmatasfp <33095685+kmatasfp@users.noreply.github.com>
2024-12-31 22:43:56 -0800

    extract url path

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,38 @@
 use crate::errors::{Error, Result};
 use std::{
-    io::Write,
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
+
+#[derive(Debug)]
+pub struct HttpRequest {
+    target: String,
+}
+
+impl TryFrom<&TcpStream> for HttpRequest {
+    type Error = Error;
+
+    fn try_from(stream: &TcpStream) -> Result<Self> {
+        let buf_reader = BufReader::new(stream);
+
+        let mut lines = buf_reader.lines();
+
+        if let Some(line) = lines.next() {
+            let request_line = line?;
+
+            let request_line_split: Vec<&str> = request_line.split_whitespace().collect();
+
+            let target = request_line_split
+                .get(1)
+                .ok_or(Error::InvalidRequest)
+                .map(|t| (*t).to_owned())?;
+
+            Ok(HttpRequest { target })
+        } else {
+            Err(Error::InvalidRequest)
+        }
+    }
+}
 
 pub struct Server {
     addr: String,
@@ -13,8 +43,12 @@ impl Server {
         Server { addr }
     }
 
-    fn send_response(stream: &mut TcpStream) -> Result<()> {
-        let result = String::from("HTTP/1.1 200 OK\r\n\r\n");
+    fn handle_request(req: &HttpRequest, stream: &mut TcpStream) -> Result<()> {
+        let result = if req.target == "/" {
+            String::from("HTTP/1.1 200 OK\r\n\r\n")
+        } else {
+            String::from("HTTP/1.1 404 Not Found\r\n\r\n")
+        };
 
         stream.write_all(result.as_bytes()).map_err(Error::Io)
     }
@@ -23,9 +57,10 @@ impl Server {
         let listener = TcpListener::bind(&self.addr)?;
 
         for stream in listener.incoming() {
-            stream
-                .map_err(|e| e.into())
-                .and_then(|mut stream| Self::send_response(&mut stream))?
+            stream.map_err(|e| e.into()).and_then(|mut stream| {
+                HttpRequest::try_from(&stream)
+                    .and_then(|req| Self::handle_request(&req, &mut stream))
+            })?
         }
 
         Ok(())
